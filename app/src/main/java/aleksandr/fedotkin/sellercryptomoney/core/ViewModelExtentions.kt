4c740cd0eb30b905ad18290f3b42927f4893a9ad
dmitriy <dmitriylosevxxx@gmail.com>
2022-12-05 15:56:49 +0300

    Добавлена обработка ошибок

--- app/src/main/java/aleksandr/fedotkin/sellercryptomoney/core/ViewModelExtentions.kt
+++ app/src/main/java/aleksandr/fedotkin/sellercryptomoney/core/ViewModelExtentions.kt
@@ -1,17 +1,16 @@
 package aleksandr.fedotkin.sellercryptomoney.core
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
-fun ViewModel.runOnBackground(block: suspend () -> Unit): Job =
+fun BaseViewModel.runOnBackground(block: suspend () -> Unit): Job =
     viewModelScope.launch(Dispatchers.Default) {
         block()
     }
 
-fun ViewModel.runOnIO(block: suspend () -> Unit): Job =
+fun BaseViewModel.runOnIO(block: suspend () -> Unit): Job =
     viewModelScope.launch(Dispatchers.IO) {
         block()
     }
